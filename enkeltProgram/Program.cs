14cb2d965443c4a2ef2b90daf624cb87ee54bcbf
Jørund Jenssen Berg <jjberg@gmail.com>
2023-09-21 06:18:13 +0200

    Funnet litt ut om sjekk av verdier i dictionaries. Basically holder det med try/catch

--- enkeltProgram/Program.cs
+++ enkeltProgram/Program.cs
@@ -1,25 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Generic;
 
-// IDictionary<string, Array> dirtyBank = new Dictionary<string, Array>() 
-// {
-//     {"rungekutta", [[3],[5]]},
-//     {"tyrannosaurus rex", [[12,14]]}
-// };
-
-// foreach(var kvp in dirtyBank)
-// {
-//     Console.WriteLine($"Nøkkel: {kvp.Key}, Verdi: {kvp.Value}");
-// }
-
-Dictionary<string, int[][]> dirtyBank = new Dictionary<string, int[][]>();
 
 // Nøkkel er ord som skal gjøres dirty
 // Første subarray er indekser på bokstaver som skal byttes ut
 // Neste subarray er indekser på bokstaver som skal bytte plass med de i første subarray
-dirtyBank["rungekutta"] = new int[][] {new int[] {3}, new int[] {5}};
+Dictionary<string, int[][]> dirtyBank = new Dictionary<string, int[][]>();
+dirtyBank["rungekutta"] = new int[][] {new int[] {3,6}, new int[] {5,9}};
+dirtyBank["tyrannosaurus rex"] = new int[][] {new int[] {12}, new int[] {14}};
 
-readDictionary(dirtyBank);
+// Console.WriteLine(dirtyBank["ole"]);
+
+
+
+// readDictionary(dirtyBank);
+
+int[][] sjekkVerdi;
+var sjekk = dirtyBank.TryGetValue("rungekutt", out sjekkVerdi);
+Console.WriteLine(sjekk);
+// foreach(var subarray in sjekkVerdi)
+// {
+//     foreach (var tall in subarray)
+//     {
+//         Console.Write(tall + ",");
+//     }
+//     Console.Write("  -  ");
+    
+// }
 
 void readDictionary(Dictionary<string, int[][]> theDict)
 {
