a1a1fcc06f693b8b24ccea9af0c1e87b63894217
Jørund Jenssen Berg <jjberg@gmail.com>
2023-09-21 05:24:44 +0200

    Initial commit

---
+++ enkeltProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+IDictionary<string, int> something = new Dictionary<string, int>() 
+{
+    {"rungekutta", 35},
+    {"tyrannosaurus rex", 1214}
+};
+
+foreach(var kvp in something)
+{
+    Console.WriteLine($"Nøkkel: {kvp.Key}, Verdi: {kvp.Value}");
+}
