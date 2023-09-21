10805e4d2978f4c35860cfc36fbfad716d72d35d
Jørund Jenssen Berg <jjberg@gmail.com>
2023-09-21 05:55:01 +0200

    Klart å lage rett format på dictionary

--- enkeltProgram/Program.cs
+++ enkeltProgram/Program.cs
@@ -1,12 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
 
-IDictionary<string, int> something = new Dictionary<string, int>() 
-{
-    {"rungekutta", 35},
-    {"tyrannosaurus rex", 1214}
-};
+// IDictionary<string, Array> dirtyBank = new Dictionary<string, Array>() 
+// {
+//     {"rungekutta", [[3],[5]]},
+//     {"tyrannosaurus rex", [[12,14]]}
+// };
 
-foreach(var kvp in something)
+// foreach(var kvp in dirtyBank)
+// {
+//     Console.WriteLine($"Nøkkel: {kvp.Key}, Verdi: {kvp.Value}");
+// }
+
+Dictionary<string, int[][]> dirtyBank = new Dictionary<string, int[][]>();
+
+// Nøkkel er ord som skal gjøres dirty
+// Første subarray er indekser på bokstaver som skal byttes ut
+// Neste subarray er indekser på bokstaver som skal bytte plass med de i første subarray
+dirtyBank["rungekutta"] = new int[][] {new int[] {3}, new int[] {5}};
+
+readDictionary(dirtyBank);
+
+void readDictionary(Dictionary<string, int[][]> theDict)
 {
-    Console.WriteLine($"Nøkkel: {kvp.Key}, Verdi: {kvp.Value}");
+    foreach (var kvp in theDict)
+    {
+        Console.Write($"{kvp.Key}: ");
+        int[][] theArray = kvp.Value;
+
+        foreach (var subarray in theArray)
+        {
+            Console.Write(" Subarray:");
+            foreach (var tall in subarray)
+            {
+                Console.Write(tall);
+            }
+        }
+
+        Console.WriteLine();
+    }
 }
